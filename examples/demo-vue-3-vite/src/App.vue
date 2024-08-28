8748a6e5e1ea4688872677b936a4a5cfc6ed6a7c
yanyue404 <xiaoyueyue165@gmail.com>
2024-08-28 18:12:46 +0800

    feat: use Test component

--- examples/demo-vue-3-vite/src/App.vue
+++ examples/demo-vue-3-vite/src/App.vue
@@ -1,14 +1,28 @@
 <template>
-  <p>Mouse: {{x}}x{{y}}</p>
+  <div>
+    <p>Mouse: {{ x }}x{{ y }}</p>
+    <Test @load="loadFn" @confirm="onConfirm" @cancel="onCancel"></Test>
+  </div>
 </template>
 
 <script>
-import { useMouse } from '@vue-demi/use-mouse'
+import { useMouse } from "@vue-demi/use-mouse";
+import Test from "./component";
 
 export default {
-  name: 'App',
+  name: "App",
+  components: {
+    Test: Test,
+  },
   setup() {
-    return useMouse()
-  }
-}
+    const { x, y } = useMouse();
+    return {
+      x,
+      y,
+      loadFn: (e) => console.log(e),
+      onConfirm: (e) => console.log(e),
+      onCancel: (e) => console.log(e),
+    };
+  },
+};
 </script>
---
+++ examples/demo-vue-3-vite/src/component/index.js
@@ -0,0 +1,89 @@
+import {
+  computed,
+  defineComponent,
+  getCurrentInstance,
+  h as hDemi,
+  isVue2,
+  isVue3,
+  onMounted,
+  onUnmounted,
+  ref,
+  unref,
+  watch,
+  watchEffect,
+} from "vue-demi";
+import "./style.css";
+if (isVue2) {
+  // Vue 2 only
+  console.log("now is vue2");
+
+  const state = ref("hello");
+
+  console.log("state: ", state);
+} else {
+  // Vue 3 only
+  console.log("now is vue3");
+}
+
+function adaptOnsV3(ons) {
+  if (!ons) return null;
+  return Object.entries(ons).reduce((ret, [key, handler]) => {
+    key = key.charAt(0).toUpperCase() + key.slice(1);
+    key = `on${key}`;
+    return { ...ret, [key]: handler };
+  }, {});
+}
+
+function h(type, options, children) {
+  if (isVue2) return hDemi(type, options, children);
+
+  const { props, domProps, on, ...extraOptions } = options;
+
+  const ons = adaptOnsV3(on);
+  const params = { ...extraOptions, ...props, ...domProps, ...ons };
+  return hDemi(type, params, children);
+}
+
+export default defineComponent({
+  name: "Test",
+  emits: ["load"],
+  data() {
+    return {
+      show: true,
+    };
+  },
+  watch: {},
+  emits: ["load", "confirm", "cancel"],
+  mounted() {
+    this.$emit("load", "mounted");
+  },
+  methods: {
+    handler(action) {
+      this.$emit(action, action);
+    },
+  },
+  render() {
+    const confirmHandler = {
+      click: () => this.handler("confirm"),
+    };
+
+    const cancelHandler = {
+      click: () => {
+        this.handler("cancel");
+        this.show = false;
+      },
+    };
+    let show = this.show;
+    if (show) {
+      return h("div", { class: "dialog-wrap" }, [
+        h("p", {}, "代码是写给人看的，附带放在机器上可以运行！"),
+        h("div", { class: "dialog-btns" }, [
+          h("button", { class: "dialog-button-confirm", on: confirmHandler }, "确定"),
+          h("button", { class: "dialog-button-cancel", on: cancelHandler }, "取消"),
+        ]),
+      ]);
+    } else {
+      return h("div", {});
+    }
+  },
+});
